c2535e7396643b0c2dbcfa31a297770ecc2c5aa3
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2021-02-15 14:35:09 +0800

    added styling, cv, portfolio page

--- js/init.js
+++ js/init.js
@@ -3,6 +3,10 @@
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
-
+    
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
